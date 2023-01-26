524be5d6f569a45875c1df7d6e5ab490b91a4483
Katrina Knight <kat.knight@newgaea.net>
2023-01-26 15:16:09 +0000

    ci: More changes on CI

--- src/main/java/dev/katcodes/mffs/test/GameTestUtils.java
+++ src/main/java/dev/katcodes/mffs/test/GameTestUtils.java
@@ -1,2 +1,11 @@
-package dev.katcodes.mffs.test;public class GameTestUtils {
+package dev.katcodes.mffs.test;
+
+import net.minecraft.gametest.framework.GameTestHelper;
+import net.minecraft.world.level.block.entity.BlockEntity;
+
+public class GameTestUtils {
+
+    public static void assertContainerEmptyAtTickCapacity(GameTestHelper helper, BlockEntity blockEntity, int ticks) {
+
+    }
 }
